b11488074283725101930a1f98366261e9a011a5
ShougotMollik <shougotmollik@gmail.com>
2025-01-04 14:30:47 +0600

    - Api services

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/ui/home_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      title: "Weather App",
+      home: HomeScreen(),
+    );
   }
 }
